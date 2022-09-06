8bce73eaf849d7caae64d48392f86a5f5c785923
MetodiMladenovski <metodi_mladenovski@yahoo.com>
2022-09-06 16:02:08 +0200

    Initial commit

---
+++ storage-management/src/test/java/mk/ukim/finki/storagemanagement/StorageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.storagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
