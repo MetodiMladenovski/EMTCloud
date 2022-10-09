5ad78e68f3cab0a8f14ebfd9da2397dfa8ca4af2
MetodiMladenovski <meto.18@outlook.com>
2022-10-09 21:05:58 +0200

    Company is made public

--- user-managment/src/main/java/mk/ukim/finki/usermanagment/xport/rest/CompanyResource.java
+++ user-managment/src/main/java/mk/ukim/finki/usermanagment/xport/rest/CompanyResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/company")
+@RequestMapping("/public/api/company")
 @AllArgsConstructor
 public class CompanyResource {
     private final CompanyClient companyClient;
